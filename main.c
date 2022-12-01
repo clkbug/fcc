57faa03d575b1b5f4341001e7db4c7daf3daa6a3
clkbug <mail@clkbug.net>
2022-12-01 21:23:34 +0900

    適当なアセンブリを吐くコードを追加

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+void print_header()
+{
+    printf("\t.file	\"main.c\"\n");
+    printf("\t.option nopic\n");
+    printf("\t.text\n");
+    printf("\t.section\t.rodata\n");
+    printf("\t.align\t4\n");
+    printf("\n");
+}
+
+void print_main()
+{
+    printf("\t.globl\tmain\n");
+    printf("\t.type	main, @function\n");
+    printf("main:\n");
+    printf("\tli\ta0, 42\n");
+    printf("\tret\n");
+}
+
 int main()
 {
-    printf("Hello, world\n");
+    print_header();
+    print_main();
     return 0;
 }
