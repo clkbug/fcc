0bdc6980815b874e3a0bb771e2702882fbdcca92
clkbug <mail@clkbug.net>
2023-03-25 22:33:31 +0900

    ローカル変数の初期化式のテストのコメントアウトを解除

--- test/local_variable_initialization.c
+++ test/local_variable_initialization.c
@@ -1,10 +1,10 @@
 int main() {
   int a = 1;
   int b = 2;
-  //   int c = 3 + b * a + b;
-  //   putchar('a' + a);
+  int c = 3 + b * a + b;
+  putchar('a' + a);
   putchar('a' + b);
-  //   putchar('a' + c);
-  //   putchar('\n');
+  putchar('a' + c);
+  putchar('\n');
   return 0;
 }
