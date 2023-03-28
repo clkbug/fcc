5ce4fa325dea5d476201d3030ee39e6b56b03925
clkbug <mail@clkbug.net>
2023-03-28 18:31:37 +0900

    typedefのテストを追加

--- test/typedef.c
+++ test/typedef.c
@@ -8,6 +8,22 @@ void test0(ptr_of_int p) { *p = *p + 30; }
 
 typedef int array_of_int[10];
 
+typedef ptr_of_int ptr_of_int2;
+typedef ptr_of_int2* ptr_of_ptr_of_int2;
+
+void test1() {
+  int a;
+  ptr_of_int p;
+  ptr_of_ptr_of_int2 pp;
+  p = &a;
+  pp = &p;
+  **pp = 10;
+  putchar(a + '0');
+  putchar(*p + '0');
+  putchar(**pp + '0');
+  putchar('\n');
+}
+
 int main() {
   int x = 10;
   array_of_int a;
@@ -22,5 +38,6 @@ int main() {
   }
   putchar('\n');
 
+  test1();
   return 0;
 }
