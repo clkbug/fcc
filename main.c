a94244c9fbd45057bcc73b1882bf5df246c9176a
clkbug <mail@clkbug.net>
2023-04-15 11:36:31 +0900

    errorなどを外に出す

--- main.c
+++ main.c
@@ -7,35 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-char *read_file(char *path) {
-  FILE *fp = fopen(path, "r");
-  if (!fp) error("cannot open %s: %s", path, strerror(errno));
-
-  // check file size
-  if (fseek(fp, 0, SEEK_END) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
-  size_t size = ftell(fp);
-  if (fseek(fp, 0, SEEK_SET) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
-
-  // read file
-  char *buf = calloc(1, size + 2);
-  fread(buf, size, 1, fp);
-
-  // "\n\0" is added to the end of the file
-  if (size == 0 || buf[size - 1] != '\n') buf[size++] = '\n';
-  buf[size] = '\0';
-  fclose(fp);
-  return buf;
-}
+#include "util.h"
 
 int last_loop_label_index = 0;
 int label_index = 0;
@@ -198,6 +170,15 @@ token_t *tokenize(char *p) {
 
     if (strncmp(p, "//", 2) == 0) {
       p += 2;
+      while (*p != '\n') {
+        p++;
+      }
+      continue;
+    }
+
+    // skip preprocessor
+    if (*p == '#') {
+      p += 1;
       while (*p != '\n') {
         p++;
       }
