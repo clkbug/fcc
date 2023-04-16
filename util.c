358644bf2e27819b76c8b95b7f015b85bb9c0e4e
clkbug <mail@clkbug.net>
2023-04-16 13:28:33 +0900

    stdinから読み込むときに失敗するシークに関するエラーチェックを無効化

--- util.c
+++ util.c
@@ -19,14 +19,15 @@ void eprintf(char *fmt, ...) {
 
 char *read_file(char *path) {
   FILE *fp = path ? fopen(path, "r") : stdin;
+  size_t size = 128 * 1024 - 2;
   if (!fp) error("cannot open %s: %s", path, strerror(errno));
 
   // check file size
-  if (fseek(fp, 0, SEEK_END) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
-  size_t size = ftell(fp);
-  if (fseek(fp, 0, SEEK_SET) == -1)
-    error("%s: fseek: %s", path, strerror(errno));
+  // if (fseek(fp, 0, SEEK_END) == -1)
+  //   error("%s: fseek: %s", path, strerror(errno));
+  // size_t size = ftell(fp);
+  // if (fseek(fp, 0, SEEK_SET) == -1)
+  //   error("%s: fseek: %s", path, strerror(errno));
 
   // read file
   char *buf = calloc(1, size + 2);
